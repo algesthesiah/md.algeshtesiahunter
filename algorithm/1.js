d393bbcbd9a4d976089a2207943cbe6f42cb25b9
liulu <541877028@qq.com>
2021-02-05 18:10:24 +0800

    fix:merge fix

---
+++ algorithm/1.js
@@ -0,0 +1,23 @@
+const a = ['flower', 'flow', 'flight'];
+function findSamePre(arr) {
+    let res = '';
+    let i = 0;
+    while (i != -1) {
+        for (let j = 0; j < arr.length; j++) {
+            const v=arr[j]
+            const strItem = v[i];
+            if (j === 0) {
+                res += strItem;
+            }
+            if (!strItem || strItem !== res[i]) {
+                res = res.slice(0, res.length - 1);
+                i === -1;
+                return res;
+            }
+        }
+        i += 1;
+    }
+    return res;
+}
+findSamePre(a);
+
